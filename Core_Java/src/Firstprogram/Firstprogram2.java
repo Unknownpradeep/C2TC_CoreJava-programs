dc1c409270d77ca940fbc7ea50cc1615dd725bd3
L540 <L540@DESKTOP-40KLJQG>
2025-10-03 19:32:41 +0530

    these are my day3 Java projects

---
+++ Core_Java/src/Firstprogram/Firstprogram2.java
@@ -0,0 +1,12 @@
+package Firstprogram;
+
+public class Firstprogram2 {
+	static int a=10;
+	public static void main(String[] args) {
+		System.out.println("Hello java");
+	}
+	public static void sub() {
+		System.out.println(a);
+	}
+
+}
